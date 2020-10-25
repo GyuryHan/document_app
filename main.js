3555611a95222781ae667407868a63b743d6b05c
GyuryHan <pos7271@daum.net>
2020-10-25 15:34:31 +0900

    security function

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 const template = require('./lib/template.js');
+//보안
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
 
 const app = http.createServer(function(request,response){
     const _url = request.url;
@@ -23,21 +26,26 @@ const app = http.createServer(function(request,response){
           });
       } else {
           fs.readdir('./data', (err, files) => {
-          fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {  //파일 읽는 방법
-            const title = queryData.id;
-            let list = template.list(files);
-            const html = template.html(title, list, 
-              `<h2>${title}</h2><p>${description}</p>`,
-                `<a href="/create">create</a> 
-                <a href="/update?id=${title}">update</a>
-                <form action="/delete_process" method="post" onsubmit="return confirm('do you want to delete this file?')">
-                  <input type="hidden" name="id" value="${title}">
-                  <input type="submit" value="delete">
-                </form>`
-              );
-            response.writeHead(200);
-            response.end(html);
-          }); 
+            const filteredId = path.parse(queryData.id).base;   // 보안코드
+            fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {  //파일 읽는 방법
+              const title = queryData.id;
+              const sanitizedTitle = sanitizeHtml(title);               // 아래와 동일
+              const sanitizedDescription = sanitizeHtml(description, {  // 사용자가 입력한 html코드(<h1>,<div>등) 삭제하여 보완, <script> 문자 제거
+                allowedTags: ['h1']    // h1태그는 허용하겠다.
+              }); 
+              let list = template.list(files);
+              const html = template.html(sanitizedTitle, list, 
+                `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
+                  `<a href="/create">create</a> 
+                  <a href="/update?id=${sanitizedTitle}">update</a>
+                  <form action="/delete_process" method="post" onsubmit="return confirm('do you want to delete this file?')">
+                    <input type="hidden" name="id" value="${sanitizedTitle}">
+                    <input type="submit" value="delete">
+                  </form>`
+                );
+              response.writeHead(200);
+              response.end(html);
+            }); 
         }); 
       }
     } else if(pathname === '/create') {
@@ -80,7 +88,8 @@ const app = http.createServer(function(request,response){
       });
     } else if(pathname === '/update') {
       fs.readdir('./data', (err, files) => {
-        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+        const filteredId = path.parse(queryData.id).base; 
+        fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
           const title = queryData.id;
           let list = template.list(files);
           const html = template.html(title, list, 
@@ -131,8 +140,10 @@ const app = http.createServer(function(request,response){
       request.on('end', function() {
         const post = qs.parse(body);  
         const id = post.id;
+        const filteredId = path.parse(id).base; 
+
         console.log("post", post);
-        fs.unlink(`data/${id}`, (err) => {
+        fs.unlink(`data/${filteredId}`, (err) => {
           response.writeHead(302, { Location: `/` });   // 302: 페이지 리다이렉션, 삭제끝나면 홈으로 보냄
           response.end();
         });  
